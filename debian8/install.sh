bdcc22b759e382192d8b0fa82de00b786d848ee0
Jaan Janesmae <jaan@janesmae.com>
2017-01-28 20:34:26 +0200

    removed functions, added check for ssh key variable change, fixed a location issue,

--- debian8/install.sh
+++ debian8/install.sh
@@ -5,11 +5,12 @@
 # Contributions are welcome. See CONTRIBUTIONS.md for more info.
 
 # Declaring a few variables
-ADD_SUDO_USER=0 # Set to 1 to add new user with sudo privileges
 USER_LOGIN=sudouser # Declare a new user login name
 USER_SSH_KEY='ssh-ed25519 AAAA...' # Users public ssh key
 
 #------------------------------------------------------------------------------#
+
+if [ "$USER_SSH_KEY" == "ssh-ed25519 AAAA..." ] ; then echo "Please add your ssh key" 1>&2 && exit 1 ; fi
 
 # This script must be run as root
 if [[ $EUID -ne 0 ]]; then
@@ -17,50 +18,53 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Function for setting up firewall
-FirewallSetup ()
-{
+echo "Running apt-get update and installing essential software"
+# Run updates, install core software
+apt-get -qq update
+apt-get -qq dist-upgrade
+apt-get -qq install build-essential vim sudo git-core curl ufw
 
-    echo "Setting up default firewall rules"
-    # Firewall default rules, deny everything
-    ufw default deny incoming
-    ufw default deny outgoing
+echo "Setting timezone to UTC"
+# Set timezone to UTC
+sudo timedatectl set-timezone UTC
 
-    # Firewall rules
-    # - allow HTTP and HTTPS traffic to the outside
-    # - limit incoming SSH connections
-    ufw allow out http
-    ufw allow out https
-    ufw allow out dns
-    ufw allow out ntp
-    ufw limit ssh
+# /***************************************/
 
-    echo "Making changes to firewall config, log to separate file and enable on boot"
-    # Changes to firewall config, log to separate file and enable on boot
-    sed -i '/~/s/^#//g' /etc/rsyslog.d/20-ufw.conf
-    sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
+echo "Setting up default firewall rules"
+# Firewall default rules, deny everything
+ufw default deny incoming
+ufw default deny outgoing
 
-    echo "Restarting affected services and enabling firewall"
-    # Restart affected services and enable Firewall
-    /etc/init.d/rsyslog restart > /dev/null
-    ufw --force enable > /dev/null
+# Firewall rules
+# - allow HTTP and HTTPS traffic to the outside
+# - limit incoming SSH connections
+ufw allow out http
+ufw allow out https
+ufw allow out dns
+ufw allow out ntp
+ufw limit ssh
 
-}
+echo "Making changes to firewall config, log to separate file and enable on boot"
+# Changes to firewall config, log to separate file and enable on boot
+sed -i '/~/s/^#//g' /etc/rsyslog.d/20-ufw.conf
+sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
 
-# Function for OpenSSH hardening
-# !!!WARNING!!! This script will disable root login via SSH
-OpenSSHHardening ()
-{
+echo "Restarting affected services and enabling firewall"
+# Restart affected services and enable Firewall
+/etc/init.d/rsyslog restart > /dev/null
+ufw --force enable > /dev/null
 
-    echo "Fix OpenSSH config"
-    # Fix OpenSSH config
-    echo "" > /etc/ssh/sshd_config
+# /***************************************/
 
-    # Hardened OpenSSH config
-    # Ref: https://cipherli.st
-    # Ref: https://stribika.github.io/2015/01/04/secure-secure-shell.html
-    # Ref: https://wiki.mozilla.org/Security/Guidelines/OpenSSH#Modern_.28OpenSSH_6.7.2B.29
-    cat > /etc/ssh/sshd_config << EOL
+echo "Fix OpenSSH config"
+# Fix OpenSSH config
+echo "" > /etc/ssh/sshd_config
+
+# Hardened OpenSSH config
+# Ref: https://cipherli.st
+# Ref: https://stribika.github.io/2015/01/04/secure-secure-shell.html
+# Ref: https://wiki.mozilla.org/Security/Guidelines/OpenSSH#Modern_.28OpenSSH_6.7.2B.29
+cat > /etc/ssh/sshd_config << EOL
 Protocol 2
 
 # Supported HostKey algorithms by order of preference.
@@ -93,66 +97,39 @@ PermitRootLogin No
 UsePrivilegeSeparation sandbox
 EOL
 
-    echo "Remove and generate new host keys"
-    # Remove and generate new host keys
-    shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
-    ssh-keygen -A
+echo "Remove and generate new host keys"
+# Remove and generate new host keys
+shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
+ssh-keygen -A
 
-    echo "Restart OpenSSH server"
-    # Restarting OpenSSH
-    /etc/init.d/ssh restart
+echo "Restart OpenSSH server"
+# Restarting OpenSSH
+/etc/init.d/ssh restart
 
-}
+# /***************************************/
 
-# Function for adding new user with sudo privileges
-NewSudoUser ()
-{
+echo "Add new user with sudo privileges"
+# Add new user
+# Make sure no password and unlocked account
+# Add user to sudoers
+useradd -m -d /home/$USER_LOGIN $USER_LOGIN
+usermod -p '*' $USER_LOGIN
+echo " ${USER_LOGIN} ALL=NOPASSWD: ALL" >> /etc/sudoers
 
-    echo "Add new user with sudo privileges"
-    # Add new user
-    # Make sure no password and unlocked account
-    # Add user to sudoers
-    useradd -m -d /home/$USER_LOGIN $USER_LOGIN
-    usermod -p '*' $USER_LOGIN
-    echo " ${USER_LOGIN} ALL=NOPASSWD: ALL" >> /etc/sudoers
+# Add public ssh key to authorized_keys for the new user
+mkdir /home/$USER_LOGIN/.ssh
+echo $USER_SSH_KEY > /home/$USER_LOGIN/.ssh/authorized_keys
+chown -R $USER_LOGIN.$USER_LOGIN /home/$USER_LOGIN/.ssh
 
-    # Add public ssh key to authorized_keys for the new user
-    mkdir /home/$USER_LOGIN/.ssh
-    echo $USER_SSH_KEY > /home/janesmae/.ssh/authorized_keys
-    chown -R $USER_LOGIN.$USER_LOGIN /home/$USER_LOGIN/.ssh
+# Set correct permissions to user home directory
+chmod go-w /home/$USER_LOGIN
+chmod 700 /home/$USER_LOGIN/.ssh
+chmod 644 /home/$USER_LOGIN/.ssh/authorized_keys
 
-    # Set correct permissions to user home directory
-    chmod go-w /home/$USER_LOGIN
-    chmod 700 /home/$USER_LOGIN/.ssh
-    chmod 644 /home/$USER_LOGIN/.ssh/authorized_keys
-
-    # The new sshd_config denies root login. Root shouldn't have ssh keys either.
-    # Comment the following 2 lines to skip that
-    shred -u ~/.ssh/*
-    rm -Rf ~/.ssh
-
-}
-
-#------------------------------------------------------------------------------#
-
-echo "Running apt-get update and installing essential software"
-# Run updates, install core software
-apt-get -qq update
-apt-get -qq dist-upgrade
-apt-get -qq install build-essential vim sudo git-core curl ufw
-
-echo "Setting timezone to UTC"
-# Set timezone to UTC
-sudo timedatectl set-timezone UTC
-
-# Setting up firewall
-FirewallSetup
-
-# OpenSSH hardening
-OpenSSHHardening
-
-# Add new user with sudo privileges
-if [ "$ADD_SUDO_USER" -eq 1 ] ; then NewSudoUser ; fi
+# The new sshd_config denies root login. Root shouldn't have ssh keys either.
+# Comment the following 2 lines to skip that
+shred -u /root/.ssh/*
+rm -Rf /root/.ssh
 
 echo "Done!"
 exit 0
